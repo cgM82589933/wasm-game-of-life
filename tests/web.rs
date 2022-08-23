7422b9e4a8fe8755d8e7ff59bfea6063707c7d25
Chris Girvin <cgm82589933@outlook.com>
2022-08-22 21:53:27 -0400

    starting to work on testing

--- tests/web.rs
+++ tests/web.rs
@@ -11,3 +11,32 @@ wasm_bindgen_test_configure!(run_in_browser);
 fn pass() {
     assert_eq!(1 + 1, 2);
 }
+
+
+#[wasm_bindgen_test]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// This is a really bad adding function, its purpose is to fail in this
+// example.
+// #[[allow(dead_code)]
+// fn bad_add(a: i32, b: i32) -> i32 {
+//     a - b
+// }
+
+#[wasm_bindgen_test]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    // #[test]
+    // fn test_bad_add() {
+        // assert_eq!(bad_add(1, 2), -1);
+    // }
+}
